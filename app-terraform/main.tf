0a5c81e5c5531bce833214d7a1dd4296cbfcb6f9
Sam Gabrail <sam.gabrail@tekanaid.com>
2020-06-11 21:43:17 +0000

    add consulconnect

--- app-terraform/main.tf
+++ app-terraform/main.tf
@@ -2,7 +2,10 @@ provider "nomad" {
   address = "http://34.70.30.144:4646"
 }
 
-// resource "nomad_job" "monitoring" {
+// resource "nomad_job" "traefik" {
 //   jobspec = "${file("${path.module}/webblogtraefik.nomad")}"
 // }
 
+resource "nomad_job" "webblogconsulconnect" {
+  jobspec = "${file("${path.module}/webblogconsulconnect.nomad")}"
+}
