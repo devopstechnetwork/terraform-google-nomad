7bc9e8fb886851582eea74e9160bca24b29ef4fd
Sam Gabrail <sam.gabrail@tekanaid.com>
2020-06-11 21:40:44 +0000

    remove traefik

--- app-terraform/main.tf
+++ app-terraform/main.tf
@@ -2,7 +2,7 @@ provider "nomad" {
   address = "http://34.70.30.144:4646"
 }
 
-resource "nomad_job" "monitoring" {
-  jobspec = "${file("${path.module}/webblogtraefik.nomad")}"
-}
+// resource "nomad_job" "monitoring" {
+//   jobspec = "${file("${path.module}/webblogtraefik.nomad")}"
+// }
 
