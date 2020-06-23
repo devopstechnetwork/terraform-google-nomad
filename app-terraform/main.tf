819027d7fdf375d37f7c29ab7b3233ef96bdbce6
samgabrail <sameh.gabrail@gmail.com>
2020-06-23 10:14:47 -0400

    nomesh

--- app-terraform/main.tf
+++ app-terraform/main.tf
@@ -6,6 +6,10 @@ provider "nomad" {
 //   jobspec = file("${path.module}/webblogtraefik.nomad")
 // }
 
-resource "nomad_job" "webblogconsulconnect" {
-  jobspec = file("${path.module}/webblogconsulconnect.nomad")
+// resource "nomad_job" "webblogconsulconnect" {
+//   jobspec = file("${path.module}/webblogconsulconnect.nomad")
+// }
+
+resource "nomad_job" "webblognomesh" {
+  jobspec = file("${path.module}/webblognomesh.nomad")
 }
